a1ce6e1934b76c9056a72b3aeee42db1eaac363f
lianghaoqiang <3250655662@qq.com>
2024-11-30 23:40:46 +0800

    lab02

---
+++ hw0b/src/list/IntList.java
@@ -0,0 +1,10 @@
+package list;
+
+/***
+ *@title IntList
+ *@description <TODO description class purpose>
+ *@author lhq49
+ *@create 2024/11/30 18:35
+ **/
+public class IntList {
+}
---
+++ hw0b/src/list/SSLList.java
@@ -0,0 +1,10 @@
+package list;
+
+/***
+ *@title SSLList
+ *@description <TODO description class purpose>
+ *@author lhq49
+ *@create 2024/11/30 20:16
+ **/
+public class SSLList {
+}
